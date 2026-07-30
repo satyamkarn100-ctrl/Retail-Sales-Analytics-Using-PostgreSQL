ea7f8f1248c06fdfa1aee348a498fc6aa813b3d8
satyamkarn100-ctrl <satyamkarn100@gmail.com>
2026-07-30 23:56:28 +0530

    completed 9 tables schema

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -34,3 +34,46 @@ CREATE TABLE olist_order_items(
 	price NUMERIC(10,2),
 	freight_value NUMERIC(10,2)
 );
+
+CREATE TABLE olist_products(
+	product_id VARCHAR(50),
+	product_category_name VARCHAR(50),
+	product_name_length INT,
+	product_description_length INT,
+	product_photos_qty INT,
+	product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
+
+CREATE TABLE olist_sellers (
+    seller_id VARCHAR(50),
+    seller_zip_code_prefix VARCHAR(10),
+    seller_city VARCHAR(50),
+    seller_state VARCHAR(50)
+);
+
+CREATE TABLE olist_order_reviews (
+    review_id VARCHAR(50),
+    order_id VARCHAR(50),
+    review_score INT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
+
+CREATE TABLE product_category_name_translation (
+    product_category_name VARCHAR(50),
+    product_category_name_english VARCHAR(50)
+);
+
+CREATE TABLE olist_geolocation (
+    geolocation_zip_code_prefix VARCHAR(20),
+    geolocation_lat NUMERIC,
+    geolocation_lng NUMERIC,
+    geolocation_city VARCHAR(100),
+    geolocation_state VARCHAR(20)
+
+);
