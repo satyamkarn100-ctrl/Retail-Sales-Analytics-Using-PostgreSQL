d4f47ed879597be2fd4034beec2a0ef9b7ec1df7
SATYAM KARN <satyamkarn100@gmail.com>
2026-09-02 23:57:48 +0530

    Add summary of orders by status with missing data counts

--- sql/04_EDA.sql
+++ sql/04_EDA.sql
@@ -12,3 +12,15 @@ SELECT
     COUNT(*) FILTER (WHERE order_delivered_customer_date IS NULL) AS missing_order_delivered_customer_date,
     COUNT(*) FILTER (WHERE order_estimated_delivery_date IS NULL) AS missing_order_estimated_delivery_date
 FROM olist_orders;
+
+
+SELECT 
+	order_status,
+	COUNT(*) AS total_orders,
+	COUNT(*) FILTER(WHERE order_approved_At IS NULL) AS missing_approved,
+	COUNT(*) FILTER(WHERE order_delivered_carrier_date IS NULL) AS missing_carrier,
+	COUNT(*) FILTER(WHERE order_delivered_customer_date IS NULL) AS missing_customer_delivery
+
+FROM olist_orders
+GROUP BY order_status
+ORDER BY total_orders DESC;
