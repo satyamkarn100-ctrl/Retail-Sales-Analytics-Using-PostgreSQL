78828c1a2f8efb71463a3c5e29136124eb09a197
SATYAM KARN <satyamkarn100@gmail.com>
2026-09-22 14:21:59 +0530

    Enhance EDA SQL with sample data and typo fix
    Added a SELECT statement to preview the first 10 orders and fixed a typo in the missing_order_purchase_timestamp calculation.

--- sql/04_EDA.sql
+++ sql/04_EDA.sql
@@ -41,12 +41,22 @@ HAVING COUNT(*)>1;
 -- Helps understand whether missing values are expected
 -- based on the order lifecycle.
 
+SELECT *
+FROM olist_orders
+LIMIT 10;
+
 SELECT
     order_status,
     COUNT(*) AS total_orders,
+	COUNT(*) FILTER(WHERE order_id IS NULL) AS missing_order_id,
+	COUNT(*) FILTER(WHERE customer_id IS NULL) AS missing_customer_id,
+	COUNT(*) FILTER(WHERE order_status IS NULL) AS missing_order_status,
+	COUNT(*) FILTER(WHERE order_purchase_timestamp IS NULL) AS mising_order_purchase_timestamp,
     COUNT(*) FILTER (WHERE order_approved_at IS NULL) AS missing_approved,
     COUNT(*) FILTER (WHERE order_delivered_carrier_date IS NULL) AS missing_carrier,
-    COUNT(*) FILTER (WHERE order_delivered_customer_date IS NULL) AS missing_customer_delivery
+    COUNT(*) FILTER (WHERE order_delivered_customer_date IS NULL) AS missing_customer_delivery,
+	COUNT(*) FILTER(WHERE order_estimated_delivery_Date IS NULL) AS missing_estimated_delivery_date
+	
 FROM olist_orders
 GROUP BY order_status
 ORDER BY total_orders DESC;
